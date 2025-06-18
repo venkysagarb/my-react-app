fdf619544b19985065e419053ebab7d8dc0145b0
venkatesh <venkatesh.sagard@gmail.com>
2025-06-18 14:33:23 +0530

    updated text content

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <h1>Welcome to the Staging Environment!</h1>
         </a>
       </header>
     </div>
